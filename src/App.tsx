1c63448f86ef9a5b96716a80947e15a165169581
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-09-11 12:04:39 +0800

    Setup header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Header from './tsx/Header.tsx'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Header />
+    
+    </>
   );
 }
---
+++ src/tsx/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <div className='flex flex-col w-full h-screen bg-[#000]'>
+
+      <div className='
+       bg-[#e0e0e0]
+        lg:w-full lg:h-20 
+        md:w-full md:h-15
+        sm:w-full sm:h-10
+        flex items-center pl-8
+        '
+      >
+        <h1 className='text-2xl font-bold italic text-[#ff6b6b]'>Ala</h1>
+        <p className='source-sans-text font-extralight'>&nbsp; – &nbsp;Capture. Share. Relive.</p>
+        
+      </div>
+
+    </div>
+  )
+}
