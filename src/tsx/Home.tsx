c62a3470f4043cfa5aa7ca475d303b399f633002
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-09-11 16:50:24 +0800

    Improve mobile responsiveness

--- src/tsx/Home.tsx
+++ src/tsx/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       {/* Ala – Capture. Share. Relive. */}
       <div
         className='
-        flex flex-col items-center justify-center w-full h-full
+        flex flex-col items-center justify-center w-full h-full z-30
         lg:flex lg:flex-col lg:items-center lg:justify-center lg:w-full lg:h-full 
         '
       >
@@ -21,7 +21,7 @@ export default function Home() {
           pb-60
 
           lg:flex lg:flex-col lg:items-center lg:justify-center lg:w-[50%]
-          lg:h-full
+          lg:h-full lg:pb-80
           '
         >
           <h1
@@ -57,45 +57,171 @@ export default function Home() {
       {/* Images */}
       <div 
         className='
-        flex items-end justify-center w-full gap-4 absolute bottom-[-275px] z-10 p-8 linear-gradient
+        flex items-end justify-center w-full absolute linear-gradient
+        lg:p-8 lg:gap-4 lg:bottom-[-200px]
         '
       >
         <div className='flex flex-col gap-4'>
-          <div className='w-[250px] h-[350px] border-0 rounded-4xl bg-[#ff6b6b] opacity-50'></div>
-          <div className='w-[250px] h-[350px] border-0 rounded-4xl bg-[#ff6b6b] opacity-50'></div>
+          <img
+            src="1.jpg"
+            alt="Image 1"
+            className='
+            w-[250px] h-[350px] border-0 rounded-4xl opacity-50
+            hidden
+            lg:block lg:opacity-100
+            '
+          />
+          <img
+            src="2.jpg"
+            alt="Image 2"
+            className='
+            w-[250px] h-[350px] border-0 rounded-4xl opacity-50
+            hidden
+            lg:block lg:opacity-100
+            '
+          />
         </div>
         
 
         <div className='mb-50'>
-          <div className='w-[250px] h-[350px] border-0 rounded-4xl bg-[#ff6b6b] opacity-50'></div>
+          <img
+            src="3.jpg"
+            alt="Image 3"
+            className='
+            w-[250px] h-[350px] border-0 rounded-4xl opacity-50
+            hidden
+            lg:block lg:opacity-100
+            '
+          />
         </div>
         
-        <div className='mb-25'>
-          <div className='w-[250px] h-[350px] border-0 rounded-4xl bg-[#ff6b6b] opacity-50'></div>
+
+
+
+
+
+
+        {/* With Mobile View */}
+        <div 
+          className='
+          flex flex-col mb-25 mr-2 gap-2 
+          lg:mr-0
+          '
+        >
+          <img
+            src="4.jpg"
+            alt="Image 4"
+            className='
+            w-[250px] h-[300px] border-0 rounded-xl opacity-50
+            lg:w-[250px] lg:h-[350px] lg:opacity-100 lg:rounded-4xl
+            '
+          />
+          <img
+            src="2.jpg"
+            alt=""
+            className='
+            w-[250px] h-[300px] border-0 rounded-xl opacity-50
+            lg:hidden lg:w-[250px] lg:h-[350px] lg:opacity-100
+            '
+          />
+          <img
+            src="3.jpg"
+            alt=""
+            className='
+            w-[250px] h-[300px] border-0 rounded-xl opacity-50
+            lg:hidden lg:w-[250px] lg:h-[350px] lg:opacity-100
+            '
+          />
         </div>
 
-        <div className='mb-25'>
-          <div className='w-[250px] h-[350px] border-0 rounded-4xl bg-[#ff6b6b] opacity-50'></div>
+        {/* With Mobile View */}
+        <div className='flex flex-col mb-25 gap-2'>
+          <img
+            src="5.jpg"
+            alt="Image 5"
+            className='
+            w-[250px] h-[300px] border-0 rounded-xl opacity-50
+            lg:w-[250px] lg:h-[350px] lg:opacity-100 lg:rounded-4xl
+            '
+          />
+          <img
+            src="5.jpg"
+            alt=""
+            className='
+            w-[250px] h-[300px] border-0 rounded-xl opacity-50
+            lg:hidden lg:w-[250px] lg:h-[350px] lg:opacity-100
+            '
+          />
+          <img
+            src="6.jpg"
+            alt=""
+            className='
+            w-[250px] h-[300px] border-0 rounded-xl opacity-50
+            lg:hidden lg:w-[250px] lg:h-[350px] lg:opacity-100
+            '
+          />
         </div>
+
+
+
+
+
+
+
+
 
         <div className='mb-50'>
-          <div className='w-[250px] h-[350px] border-0 rounded-4xl bg-[#ff6b6b] opacity-50'></div>
+          <img
+            src="6.jpg"
+            alt="Image 6"
+            className='
+            w-[250px] h-[350px] border-0 rounded-4xl opacity-50
+            hidden
+            lg:block lg:opacity-100
+            '
+          />
         </div>
 
         <div className='flex flex-col gap-4'>
-          <div className='w-[250px] h-[350px] border-0 rounded-4xl bg-[#ff6b6b] opacity-50'></div>
-          <div className='w-[250px] h-[350px] border-0 rounded-4xl bg-[#ff6b6b] opacity-50'></div>
+          <img
+            src="7.jpg"
+            alt="Image 7"
+            className='
+            w-[250px] h-[350px] border-0 rounded-4xl opacity-50
+            hidden
+            lg:block lg:opacity-100
+            '
+          />
+          <img
+            src="8.jpg"
+            alt="Image 8"
+            className='
+            w-[250px] h-[350px] border-0 rounded-4xl opacity-50
+            hidden
+            lg:block lg:opacity-100
+            '
+          />
         </div>
       </div>
 
       {/* Gradient Fade */}
       <div
-        className="
+        className='
           pointer-events-none
           absolute bottom-0 left-0 w-full h-40 
           bg-gradient-to-t from-white to-transparent
+          z-10
+        '
+      ></div>
+
+      {/* Gradient Fade */}
+      <div
+        className='
+          lg:hidden pointer-events-none
+          absolute left-0 w-full h-full
+          bg-black/25
           z-20
-        "
+        '
       ></div>
 
 
