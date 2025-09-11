1afe198ea3bfb81e633010a12cd6a548abf2058b
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-09-11 22:18:02 +0800

    Update index.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // Import the plugin
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Add the plugin here
+    tailwindcss(),
   ],
-});
+})
