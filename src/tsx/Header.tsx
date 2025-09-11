4e85bcd44e10205a34f50a073b86bf2055038f75
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-09-11 14:25:10 +0800

    Add responsiveness to home

--- src/tsx/Header.tsx
+++ src/tsx/Header.tsx
@@ -2,36 +2,49 @@ export default function Header() {
   return (
     <div
       className='
-      absolute bg-[#fff]
-      lg:w-full lg:h-20
+      hidden
+      md:block md:absolute md:bg-[#fff] md:shadow-lg
       md:w-full md:h-20
-      sm:w-full sm:h-20
+      lg:block lg:absolute lg:bg-[#fff] lg:shadow-lg
+      lg:w-full lg:h-20
       '
     >
-
+      {/* Header Content */}
       <div
         className='
-        flex
-        lg:w-full lg:h-20
+        flex items-center
         md:w-full md:h-20
-        sm:w-full sm:h-20
+        lg:w-full lg:h-20
         '
       >
-
         {/* Left Part */}
-        <div className='flex items-center w-[50%] h-full pl-8'>
-          <h1 className='text-2xl font-bold italic text-[#ff6b6b]'>Ala</h1>
-          <p className='source-sans-text font-semibold'>&nbsp; – &nbsp;Capture. Share. Relive.</p>
+        <div
+          className='
+          flex items-center justify-start
+          md:w-[50%] md:h-full md:pl-8
+          lg:w-[50%] lg:h-full lg:pl-12
+          '
+        >
+          <h1 className='text-4xl font-bold italic text-[#ff6b6b]'>Ala</h1>
         </div>
 
         {/* Right Part */}
-        <div className='flex items-center justify-end w-[50%] h-full gap-2 pr-8'>
-          <button className='bg-[#ff6b6b] text-[#fff] text-sm font-medium p-4 rounded-[20px] cursor-pointer'>Organizer</button>
-          <button className='bg-[#e0e0e0] text-[#000] text-sm font-medium p-4 rounded-[20px] cursor-pointer'>Guest</button>
+        <div
+          className='
+          md:flex md:items-center md:justify-end md:w-[50%] md:h-full
+          md:gap-2 md:pr-8
+          lg:flex lg:items-center lg:justify-end lg:w-[50%] lg:h-full
+          lg:gap-2 lg:pr-12
+          '
+        >
+          <button className='bg-[#ff6b6b] text-[#fff] text-sm font-medium p-4 rounded-[20px] cursor-pointer'>
+            Organizer
+          </button>
+          <button className='bg-[#e0e0e0] text-[#000] text-sm font-medium p-4 rounded-[20px] cursor-pointer'>
+            Guest
+          </button>
         </div>
-
       </div>
-
     </div>
   );
 }
--- src/tsx/Home.tsx
+++ src/tsx/Home.tsx
@@ -1,6 +1,54 @@
 export default function Home() {
   return (
-    <div className='w-full h-screen bg-black/50'>
+    <div className='flex w-full h-screen bg-[#fff]'>
+
+      {/* Left Part */}
+      <div
+        className='
+        hidden
+        w-[50%] h-full bg-amber-300
+        lg:block
+        '
+      >
+
+      </div>
+
+      {/* Right Part */}
+      <div 
+        className='
+        flex flex-col w-full items-center justify-center
+        lg:flex lg:flex-col lg:items-center lg:justify-center lg:w-[50%]
+        lg:h-full
+        '
+      >
+
+        <h1
+          className='
+         text-[#ff6b6b] italic font-bold text-[10rem]
+          md:text-[10rem]
+          lg:text-[15rem]
+          '
+        >
+          Ala
+        </h1>
+
+        <div
+          className='
+          flex font-light mt-[-50px] text-2xl
+          md:mt-[-50px] md:text-3xl
+          lg:mt-[-75px] lg:text-4xl
+          '
+        >
+          <h1>Capture. &nbsp;</h1>
+          <h1>Share. &nbsp;</h1>
+          <h1>Relive.</h1>
+        </div>
+        
+        
+
+
+
+      </div>
 
     </div>
   )
